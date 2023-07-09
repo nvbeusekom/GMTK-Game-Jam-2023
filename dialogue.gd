00a48474833fda4d158ac69d8a882054e13255ee
Jules <jules.derks@gmail.com>
2023-07-09 12:22:58 +0200

    Added dialogue

--- dialogue.gd
+++ dialogue.gd
@@ -3,19 +3,64 @@ extends Node2D
 #ID 0: start of game
 #ID 1: Hero loses
 #ID 2: Hero wins, first shop visit
-#ID 3: Dark knight loses
-#ID 4: Dark knight wins
+#ID 3: Dark knight enters dungeon
+#ID 4: Dark knight loses
+#ID 5: Dark knight reaches hero
+#ID 6: Dark knight wins
 
 var messages = [
-	[
-	["Dark knight!",0], 
-	["Your dark days are numbered!",0], 
-	["I'm coming to get you!",0], 
-	["It's the hero!",1],
-	["I better get my dungeon defenses in order.",1],
-	["I shouldn't forget my money printers. Otherwise, I could run out of money!",1],
-	["Hey! Printing money is illegal!",0]
-]
+	[#ID 0
+		["DARK KNIGHT!",0], 
+		["Your dark days are numbered!",0], 
+		["I'm coming to get you!",0], 
+		["It's the HERO!",1],
+		["I better get my dungeon defenses in order.",1],
+		["I shouldn't forget my money printers. Otherwise, I could run out of money!",1],
+		["Hey! Printing money is illegal!",0]
+	],
+	[#ID 1
+		["Hahaha! Take that HERO!",1],
+		["I have been defeated...",0],
+		["But a HERO never gives up!",0],
+		["I will be back!",0],
+		["Hahahaha! Good luck, HERO!",1]
+	],
+	[#ID 2
+		["N-nn-no! This is impossible!",1],
+		["I warned you DARK KNIGHT.",0], 
+		["Your dark days are over!",0], 
+		["I, the HERO, control your dungeon now!",0], 
+		["Begone!",0], 
+		["Damn, that HERO is strong...",1], 
+		["To take back control I have to run through my own dungeon!",1], 
+		["I should check the Shoppe in town to find something to get stronger...",1], 
+	],
+	[#ID 3
+		["HERO! I am here to take back my dungeon!",1],
+		["...",1],
+		["You hypocrit! You're using my money printers!",1],
+		["They're just so convenient...",0]
+	],
+	[#ID 4
+		["Argh... Defeated again.", 1],
+		["I need to get stronger.",1],
+		["Back to the Shoppe it is!",1]
+	],
+	[#ID 5
+		["Do you really think you can defeat me, DARK KNIGHT?",0],
+		["The HERO always wins!",0],
+		["CHAAAAAAAARGE!!!",1]
+	],
+	[#ID 6
+		["Take that, HERO!",1],
+		["Nooo!",0],
+		["How can I lose?",0],
+		["The HERO never loses!",0],
+		["Wait... Am I the BADDIE?",0],
+		["I need to think about this...",0],
+		["Bye DARK KNIGHT",0],
+		["Bye HERO, thanks for playing, hehe",1]
+	]
 ]
 
 signal dialogueFinished
